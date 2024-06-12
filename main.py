1402c324f1f2e09f5fc4c5dca3f16bdd5a1bc518
Urek Mazino <urekmazth@gmail.com>
2024-06-12 08:53:59 +0100

    chore: Improve getting repositories for all page

--- main.py
+++ main.py
@@ -7,35 +7,60 @@ load_dotenv()
 
 access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
 
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {access_token}",
-    "X-GitHub-Api-Version": "2022-11-28"
-}
-
-url = "https://api.github.com/users/urekmazth/repos"
+headers = None
+if access_token:
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
 
 payload = {}
 
-# or requests.request(url, method='GET',.....)
-response = requests.get(url, headers=headers, data=payload)
 
-repos = response.json()
-repositories = []
+def get_all_user_repositories(username: str):
+    """
+    Uses GITHUB REST API to get all repositories for a given user.
+    Retrieve information of interest
+    """
+    url = f"https://api.github.com/users/{username}/repos"
 
-for repo in repos:
-    info = {
-        "id": repo.get("id"),
-        "name": repo.get("name"),
-        "url": repo.get("html_url"),
-        "description": repo.get("description"),
-        "language": repo.get("language"),
-        "stars": repo.get("stargazers_count"),
-        "forks": repo.get("forks_count"),
-        "fork": str(repo.get("fork")),
-        "created_at": repo.get("created_at"),
-    }
+    repositories = []
+    page = 1
 
-    repositories.append(info)
+    while True:
+        # or requests.request(url, method='GET',.....)
+        params = {"page": page, "per_page": 100}
+        response = requests.get(url, headers=headers,
+                                data=payload, params=params)
 
-print(repositories[0])
+        # raise exception if an error is encountered
+        response.raise_for_status()
+
+        repos = response.json()
+
+        if not repos:
+            break
+
+        for repo in repos:
+            info = {
+                "id": repo.get("id"),
+                "name": repo.get("name"),
+                "url": repo.get("html_url"),
+                "description": repo.get("description"),
+                "language": repo.get("language"),
+                "stars": repo.get("stargazers_count"),
+                "forks": repo.get("forks_count"),
+                "fork": str(repo.get("fork")),
+                "created_at": repo.get("created_at"),
+            }
+
+            repositories.append(info)
+        page += 1
+
+    print(repositories[0])
+    print(page)
+    print(len(repositories))
+
+
+get_all_user_repositories("urekmazth")
